307a70fbcdae6aec5da65636cd54c062205365af
GautamGoyal2341 <gautamgoyal2341@gmail.com>
2022-06-01 18:10:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// (function(
+
+// ))();
+
+(function(){
+
+
+    let screen = document.querySelector('.screen');
+    let button = document.querySelectorAll('.btn');
+    let clear  = document.getElementById('.btn-clear');
+    let ac =     document.getElementById('.btn-equal');
+
+
+
+})();
+
+
